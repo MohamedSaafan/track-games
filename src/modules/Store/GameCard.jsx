3ba1d7c666944ec20c488d1e8d51973e9615d83b
mozes32 <moussaebrahem32@gmail.com>
2022-01-07 13:29:52 +0200

    Change cards

--- src/modules/Store/GameCard.jsx
+++ src/modules/Store/GameCard.jsx
@@ -1,17 +1,21 @@
 import { Card } from "react-bootstrap";
-import { MainLogo } from "../../assets";
+import { FaEthereum } from "react-icons/fa";
 
 const GameCard = ({ cardImage, cardTitle, points }) => {
   return (
     <Card>
       <Card.Img variant="top" src={cardImage} />
-      <Card.Body className="bg-secondary text-white">
-        <Card.Title>{cardTitle}</Card.Title>
-        <Card.Text className="font-xss">
-          <img src={MainLogo} className="me-2" width={15} height={20} alt={cardTitle} />
-          {points}
-        </Card.Text>
-        <Card.Text className="font-xss">Mirandus</Card.Text>
+      <Card.Body>
+        <Card.Title className="font-md">{cardTitle}</Card.Title>
+        <div className="game-price-section font-sm mt-3">
+          <span className="text-muted font-sm">Price</span>
+          <div className="game-price">
+            <span className="d-flex align-items-center gap-2">
+              <FaEthereum className="ethereum-icon" /> 333 ETH
+            </span>
+            <span className="text-muted">= $ 181,1917.90</span>
+          </div>
+        </div>
       </Card.Body>
     </Card>
   );
